6a16a3c9c6f738b44792baad681b2b979cc408ee
Zygy93 <amazinzygy@gmail.com>
2018-03-04 15:26:14 -0600

    restarted, needed a fresh start

--- server/public/scripts/services/name-service.js
+++
@@ -1,9 +0,0 @@
-// CHANGE SERVICE NAME AS NEEDED FOR YOUR PROJECT
-myApp.service('SERVICENAME', ['$http', function($http){
-	console.log('SERVICENAME loaded');
-
-	const self = this;
-	
-	self.message = "Hello from the SERVICENAME!";
-
-}]);
