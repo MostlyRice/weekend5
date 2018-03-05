9f66fcc6dc724ec1b39e4ccdac06c2faad038072
Zygy93 <amazinzygy@gmail.com>
2018-03-04 21:01:27 -0600

    finished routers and service

--- server/public/scripts/services/swapiService.js
+++ server/public/scripts/services/swapiService.js
@@ -2,7 +2,7 @@ myApp.service('SwapiService', ['$http', function($http) {
 	console.log('SwapiService Locked and loaded!');
 
 	const self = this;
-	
+
 	self.message = "Welcome to the SwapiService!";
 
 	// THIS HANDLES CREATING AN ARRAY INSIDE AN OBJECT
@@ -18,7 +18,46 @@ myApp.service('SwapiService', ['$http', function($http) {
 			console.log('self.searchResults.list', self.searchResults.list);
 		})
 	}
+	// THIS HANDLES GETTING THE VARIABLE
+	self.getVariable = () => {
+		$http.get('https://swapi.co/api/')
+		.then((response) => {
+			self.variables.list = response.data;
+		})
+	}
 
+	// THIS HANDLES GETTING FAVORITES
+	self.getFavorites = () => {
+		$http.get('/favorites')
+		.then((response) => {
+			self.favorites.list = response.data;
+			console.log('self.favorites.list', self.favorites.list);
+		})
+		.catch((response) => {
+			console.log('Error GETting getFavorites!');
+		})
+	}
+
+	// THIS HANDLES POSTING FAVORITED SEARCH RESULTS
+	self.addToFavorites = (favorite) => {
+		$http.post('/favorites', favorite)
+		.then((response) => {
+		})
+		.catch((response) => {
+			console.log('Error POSTing addToFavorites!');
+		})
+	}
+
+	// THIS HANDLES DELETING A FAVORITE
+	self.deleteFavorite = (favoriteId) => {
+		$http.delete(`/favorites/${favoriteId}`)
+		.then((response) => {
+			self.getFavorites();
+		})
+		.catch((response) => {
+			console.log('Error DELETing:', response);
+		})
+	}
 
 	self.getFavorites();
 }]);
--- server/routes/favoriteRouter.js
+++ server/routes/favoriteRouter.js
@@ -7,5 +7,52 @@ const favoritesSchemaModule = require('../modules/schemaModule');
 let favoritesSchema = favoritesSchemaModule;
 let Favorite = mongoose.model('Favorite', favoritesSchema);
 
+// START GET ROUTE
+router.get('/', (req, res) => {
+    Favorite.find({'favorited': 'true'}, (err, foundFavorites) => {
+        if (err) {
+            console.log('Error Finding:', err);
+            res.sendStatus(500);
+        }
+        else {
+            res.send(foundFavorites);
+        }
+    })
+})
+// END GET ROUTE
+
+// START POST ROUTE
+router.post('/', (req, res) => {
+    let favoriteToAdd = new Favorite(req.body);
+
+    favoriteToAdd.save((err, savedFavorite) => {
+        if (err) {
+            console.log('Error Saving:', err);
+            res.sendStatus(500);
+        }
+        else {
+            res.sendStatus(201);
+        }
+    })
+})
+// END POST ROUTE
+
+// START DELETE ROUTE
+router.delete('/:id', (req, res) => {
+    let favoriteId = req.params.id;
+    Favorite.findByIdAndRemove(
+        {'_id': favoriteId},
+        (err, removedDocument) => {
+            if (err) {
+                console.log('Error Deleting', err);
+                res.sendStatus(500);
+            }
+            else {
+                res.sendStatus(200);
+            }
+        }
+    )
+})
+// END DELETE ROUTE
 
 module.exports = router;
